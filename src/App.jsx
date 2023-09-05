90851a1ccbd3449933864f6c56feed392003d2b2
Bea.Anderson <Bea.Anderson@fdmgroup.com>
2023-09-05 12:20:01 +0100

    added param passing

--- src/App.jsx
+++ src/App.jsx
@@ -12,20 +12,19 @@ import SearchResults from "./pages/SearchResults";
 import Home from "./pages/Home";
 import HeaderAuth from "./Components/HeaderAuth";
 
-
 function App() {
   return (
     <Router>
       <div>
         <HeaderAuth />
         <Routes>
-          <Route path="/" element={<Home/>}/>
+          <Route path="/" element={<Home />} />
           <Route path="/consultants" element={<AllConsultants />} />
           <Route path="consultants/:id" element={<ConsultantDetails />} />
           <Route path="login" element={<Login />} />
           <Route path="accessdenied" element={<AccessDenied />} />
           <Route path="search" element={<SearchForm />} />
-          <Route path="searchresults/:stream" element={<SearchResults />} />
+          <Route path="searchresults" element={<SearchResults />} />
         </Routes>
       </div>
     </Router>
